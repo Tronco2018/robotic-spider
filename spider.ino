7d181e916ba53406a9a87869d7a7c6c9b4462b1d
tronco2018 <tronco2018dev@gmail.com>
2025-11-12 16:35:10 +0100

    added up and own functions

--- spider.ino
+++ spider.ino
@@ -1,40 +1,79 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
 
+// Motion range
+
 #define SERVOMAX 150
 #define SERVOMIN 600
 
 Adafruit_PWMServoDriver pca = Adafruit_PWMServoDriver();
 
 // SERVO
-const short servoPiede = 0;
-const short servoTibia = 1;
+const short gamba1[] = {0, 1, 2};
+const short gamba2[] = {3, 4, 5};
+const short gamba3[] = {6, 7, 8};
+const short gamba4[] = {9, 10, 11};
 
-const short offsetPiede = 55;
-const short offsetTibia = 60;
+
+// MOVING FUNCTIONS
+
+void rise() {
+  // Range offsets
+  const short offsetPiede = 55;
+  const short offsetTibia = 60;
+
+  for (int angle = 150; angle >= 30; angle -= 5){
+    // Tibia
+    uint16_t pulse = map(angle + offsetTibia, 0, 180, SERVOMIN, SERVOMAX);
+    pca.setPWM(gamba1[1], 0, pulse);
+    pca.setPWM(gamba2[1], 0, pulse);
+    pca.setPWM(gamba3[1], 0, pulse);
+    pca.setPWM(gamba4[1], 0, pulse);
+
+    // Piede
+    pulse = map(angle + offsetPiede, 0, 180, SERVOMIN, SERVOMAX);
+    pca.setPWM(gamba1[0], 0, pulse);
+    pca.setPWM(gamba2[0], 0, pulse);
+    pca.setPWM(gamba3[0], 0, pulse);
+    pca.setPWM(gamba4[0], 0, pulse);
+
+    delay(20);
+  }
+}
+
+void down() { //IMPORTANT: motore piede fa extra movimento
+  // Range offsets
+  const short offsetPiede = 55;
+  const short offsetTibia = 60;
+
+  for (int angle = 30; angle <= 150; angle += 5){
+    // Tibia
+    uint16_t pulse = map(angle + offsetTibia, 0, 180, SERVOMIN, SERVOMAX);
+    pca.setPWM(gamba1[1], 0, pulse);
+    pca.setPWM(gamba2[1], 0, pulse);
+    pca.setPWM(gamba3[1], 0, pulse);
+    pca.setPWM(gamba4[1], 0, pulse);
+
+    // Piede
+    pulse = map(angle + offsetPiede, 0, 180, SERVOMIN, SERVOMAX);
+    pca.setPWM(gamba1[0], 0, pulse);
+    pca.setPWM(gamba2[0], 0, pulse);
+    pca.setPWM(gamba3[0], 0, pulse);
+    pca.setPWM(gamba4[0], 0, pulse);
+
+    delay(20);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  // Begin di pca
   pca.begin();
   pca.setPWMFreq(50);
 
+  rise();
+  delay(100);
+  down();
   
-  
-  for (int angle = 150; angle >= 30; angle -= 5){
-    uint16_t pulse = map(angle + offsetTibia, 0, 180, SERVOMIN, SERVOMAX);
-    pca.setPWM(servoTibia, 0, pulse);
-    pulse = map(angle + offsetPiede, 0, 180, SERVOMIN, SERVOMAX);
-    pca.setPWM(servoPiede, 0, pulse);
-    delay(70);
-  }
-  for (int angle = 30; angle <= 150; angle += 5){
-    uint16_t pulse = map(angle + offsetTibia, 0, 180, SERVOMIN, SERVOMAX);
-    
-    pca.setPWM(servoTibia, 0, pulse);
-    pulse = map(angle + offsetPiede, 0, 180, SERVOMIN, SERVOMAX);
-    pca.setPWM(servoPiede, 0, pulse);
-    delay(70);
-  }
 }
 
 void loop() {
