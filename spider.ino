c168b4f780c19bfd80d8d550ba72786f6459fb00
tronco2018 <tronco2018dev@gmail.com>
2025-11-19 16:33:05 +0100

    Big update
    Added setServoDegrees function and leg-related control functions. Refractored rise and down functions which are still incomplete. Added a log system for the leg position. Fixed the SERVOMAX and SERVOMIN constants with functioning values

--- spider.ino
+++ spider.ino
@@ -1,68 +1,107 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
 
+/*
+==NOTES==
+- Untested/unfinished functions: rise, down
+*/
+
 // Motion range
 
 #define SERVOMAX 150
-#define SERVOMIN 600
+#define SERVOMIN 550
 
 Adafruit_PWMServoDriver pca = Adafruit_PWMServoDriver();
 
 // SERVO
-const short gamba1[] = {0, 1, 2};
-const short gamba2[] = {3, 4, 5};
-const short gamba3[] = {6, 7, 8};
-const short gamba4[] = {9, 10, 11};
+int gamba1[] = {0, 1, 2};
+int gamba2[] = {3, 4, 5};
+int gamba3[] = {6, 7, 8};
+int gamba4[] = {9, 10, 11};
 
 
 // MOVING FUNCTIONS
 
-void rise() {
-  // Range offsets
-  const short offsetPiede = 55;
-  const short offsetTibia = 60;
 
-  for (int angle = 150; angle >= 30; angle -= 5){
-    // Tibia
-    uint16_t pulse = map(angle + offsetTibia, 0, 180, SERVOMIN, SERVOMAX);
-    pca.setPWM(gamba1[1], 0, pulse);
-    pca.setPWM(gamba2[1], 0, pulse);
-    pca.setPWM(gamba3[1], 0, pulse);
-    pca.setPWM(gamba4[1], 0, pulse);
 
-    // Piede
-    pulse = map(angle + offsetPiede, 0, 180, SERVOMIN, SERVOMAX);
-    pca.setPWM(gamba1[0], 0, pulse);
-    pca.setPWM(gamba2[0], 0, pulse);
-    pca.setPWM(gamba3[0], 0, pulse);
-    pca.setPWM(gamba4[0], 0, pulse);
+void setServoDegrees(uint8_t channel, int degrees) {
+  // Limita i gradi al range -90 / +90
+  if (degrees > 90) degrees = 90;
+  if (degrees < -90) degrees = -90;
 
-    delay(20);
-  }
+  // Mappa i gradi in un valore PWM relativo al centro = 400
+  // -90° → SERVOMIN
+  //   0° → 400
+  // +90° → SERVOMAX
+  int pulse = map(degrees, -90, 90, SERVOMIN, SERVOMAX);
+
+  pca.setPWM(channel, 0, pulse);
 }
 
-void down() { //IMPORTANT: motore piede fa extra movimento
+// == START LEG FUNCTIONS ==
+
+void raiseLeg(int leg[]){
+  setServoDegrees(leg[0], 50);
+  setServoDegrees(leg[1], 0);
+  Serial.println("YState: raised");
+}
+
+void resetLeg(int leg[]){
+  setServoDegrees(leg[0], 60);
+  setServoDegrees(leg[1], -20);
+  Serial.println("YState: neutral");
+}
+
+void legForward(int leg[]){
+  raiseLeg(leg);
+  delay(500);
+  setServoDegrees(leg[2], 35);
+  delay(1000);
+  Serial.println("XState: forward");
+  resetLeg(leg);
+}
+void legBackwards(int leg[]){
+  raiseLeg(leg);
+  delay(500);
+  setServoDegrees(leg[2], -35);
+  Serial.println("XState: backwards");
+  delay(1000);
+  resetLeg(leg);
+}
+void legNeutral(int leg[]){
+  raiseLeg(leg);
+  delay(500);
+  setServoDegrees(leg[2], 0);
+  Serial.println("XState: neutral");
+  delay(1000);
+  resetLeg(leg);
+}
+
+// == END LEG FUNCTIONS ==
+
+void rise() {
   // Range offsets
-  const short offsetPiede = 55;
-  const short offsetTibia = 60;
+  setServoDegrees(gamba1[0], 60);
+  setServoDegrees(gamba1[1], -20);
+}
 
-  for (int angle = 30; angle <= 150; angle += 5){
-    // Tibia
-    uint16_t pulse = map(angle + offsetTibia, 0, 180, SERVOMIN, SERVOMAX);
-    pca.setPWM(gamba1[1], 0, pulse);
-    pca.setPWM(gamba2[1], 0, pulse);
-    pca.setPWM(gamba3[1], 0, pulse);
-    pca.setPWM(gamba4[1], 0, pulse);
+void down() {
+  setServoDegrees(gamba1[0], 90);
+  setServoDegrees(gamba1[1], 30);
+}
 
-    // Piede
-    pulse = map(angle + offsetPiede, 0, 180, SERVOMIN, SERVOMAX);
-    pca.setPWM(gamba1[0], 0, pulse);
-    pca.setPWM(gamba2[0], 0, pulse);
-    pca.setPWM(gamba3[0], 0, pulse);
-    pca.setPWM(gamba4[0], 0, pulse);
+void tpose(){
+    legNeutral(gamba1);
+    setServoDegrees(gamba1[0], 0);
+    setServoDegrees(gamba1[1], 0);
+    Serial.println("YState: T-POSE");
 
-    delay(20);
-  }
+    setServoDegrees(gamba1[2], 0);
+    Serial.println("XState: T-POSE");
+
+    setServoDegrees(gamba2[0], 0);
+    setServoDegrees(gamba2[1], 0);
+    setServoDegrees(gamba2[2], 0);
 }
 
 void setup() {
@@ -70,9 +109,20 @@ void setup() {
   pca.begin();
   pca.setPWMFreq(50);
 
-  rise();
-  delay(100);
-  down();
+  Serial.begin(9600);
+
+  /*
+  legNeutral(gamba1);
+  delay(1000);
+  legForward(gamba1);
+  */
+  legNeutral(gamba1);
+  Serial.println("=============");
+  delay(2000);
+  legForward(gamba1);
+  Serial.println("=============");
+  delay(2000);
+  tpose();
   
 }
 
