e11426ed5c849b0c3c762374b8db869af0baf5b9
Tronco2018 <tronco2018dev@gmail.com>
2025-12-15 19:55:36 +0100

    Update

--- spider/spider.ino
+++ spider/spider.ino
@@ -1,4 +1,6 @@
 #include <Adafruit_PWMServoDriver.h>
+
+
 #include <Wire.h>
 #include <WiFi.h>
 #include <WebServer.h>
@@ -6,6 +8,8 @@
 // ====== CONFIGURAZIONE ACCESS POINT ======
 const char* ap_ssid = "ESP32-AP";
 const char* ap_password = "12345678";   // deve essere almeno 8 caratteri
+
+bool status = false;
 
 const char* html = R"(
   <!DOCTYPE html>
@@ -177,26 +181,20 @@ void resetLeg(int leg[]){
 
 void legForward(int leg[]){
   raiseLeg(leg);
-  delay(500);
   setServoDegrees(leg[2], 35);
-  delay(1000);
   Serial.println("XState: forward");
   resetLeg(leg);
 }
 void legBackwards(int leg[]){
   raiseLeg(leg);
-  delay(500);
   setServoDegrees(leg[2], -35);
   Serial.println("XState: backwards");
-  delay(1000);
   resetLeg(leg);
 }
 void legNeutral(int leg[]){
   raiseLeg(leg);
-  delay(500);
   setServoDegrees(leg[2], 0);
   Serial.println("XState: neutral");
-  delay(1000);
   resetLeg(leg);
 }
 
@@ -206,11 +204,77 @@ void rise() {
   // Range offsets
   setServoDegrees(gamba1[0], 60);
   setServoDegrees(gamba1[1], -20);
+
+  setServoDegrees(gamba2[0], 60);
+  setServoDegrees(gamba2[1], -20);
+
+  setServoDegrees(gamba3[0], 60);
+  setServoDegrees(gamba3[1], -20);
+
+  setServoDegrees(gamba4[0], 60);
+  setServoDegrees(gamba4[1], -20);
 }
 
 void down() {
   setServoDegrees(gamba1[0], 90);
-  setServoDegrees(gamba1[1], 30);
+  setServoDegrees(gamba1[1], 50);
+
+  setServoDegrees(gamba2[0], 90);
+  setServoDegrees(gamba2[1], 50);
+
+  setServoDegrees(gamba3[0], 90);
+  setServoDegrees(gamba3[1], 50);
+
+  setServoDegrees(gamba4[0], 90);
+  setServoDegrees(gamba4[1], 50);
+}
+void TESTdownSmooth(){
+  for (int i = 0; i < 90; i++){
+    setServoDegrees(gamba1[0], i);
+    setServoDegrees(gamba1[1], i >= 40 ? i-40 : 0);
+
+    setServoDegrees(gamba2[0], i);
+    setServoDegrees(gamba2[1], i >= 40 ? i-40 : 0);
+
+    setServoDegrees(gamba3[0], i);
+    setServoDegrees(gamba3[1], i >= 40 ? i-40 : 0);
+
+    setServoDegrees(gamba4[0], i);
+    setServoDegrees(gamba4[1], i >= 40 ? i-40 : 0);
+    delay(10);
+  }
+}
+void downSmooth(){
+  for (int i = 90; i > -50; i--){
+    setServoDegrees(gamba1[0], i);
+    setServoDegrees(gamba1[1], i >= 40 ? i-40 : 0);
+
+    setServoDegrees(gamba2[0], i);
+    setServoDegrees(gamba2[1], i >= 40 ? i-40 : 0);
+
+    setServoDegrees(gamba3[0], i);
+    setServoDegrees(gamba3[1], i >= 40 ? i-40 : 0);
+
+    setServoDegrees(gamba4[0], i);
+    setServoDegrees(gamba4[1], i >= 40 ? i-40 : 0);
+    delay(10);
+  }
+}
+
+
+void riseSmooth(){
+  for (int i = 90; i > -50; i--){
+      if (i >= 0) setServoDegrees(gamba1[1], i-40);
+      setServoDegrees(gamba1[0], i > 0 ? i : -i);
+      delay(20);
+  }
+}
+void resetFromRise(){
+  for (int i = -50; i < 90; i++){
+      if (i >= 0) setServoDegrees(gamba1[1], i-40);
+      setServoDegrees(gamba1[0], i > 0 ? i : -i);
+      delay(20);
+  }
 }
 
 void tpose(){
@@ -225,6 +289,56 @@ void tpose(){
     setServoDegrees(gamba2[0], 0);
     setServoDegrees(gamba2[1], 0);
     setServoDegrees(gamba2[2], 0);
+}
+void saluto(int gamba[]){
+  setServoDegrees(gamba[0], -15);
+  setServoDegrees(gamba[1], 30);
+}
+void demo(){
+  tpose();
+  delay(2000);
+  saluto(gamba1);
+  saluto(gamba2);
+  delay(1500);
+  
+  for (int i = -90; i < 90; i++){
+    setServoDegrees(gamba1[0], i);
+    setServoDegrees(gamba1[1], i);
+    delay(30);
+  }
+}
+void demoSeria(){
+  tpose();
+  for (int i = 0; i > -60; i--){
+    setServoDegrees(gamba1[0], i);
+    setServoDegrees(gamba1[1], i);
+    delay(30);
+  }
+  for (int i = -60; i < 50; i++){
+    setServoDegrees(gamba1[0], i);
+    setServoDegrees(gamba1[1], i);
+    delay(20);
+  }
+  for (int i = 50; i > 0; i--){
+    setServoDegrees(gamba1[0], i);
+    setServoDegrees(gamba1[1], i);
+    delay(20);
+  }
+}
+void demoCamminata(){
+  tpose();
+  for (int i = 0; i < 60; i++){
+    setServoDegrees(gamba1[1], i);
+    if (i == 20){
+      for (int j = 0; j > -20; j--){
+        setServoDegrees(gamba1[0], -j);
+        delay(20);
+      }
+    }
+    
+    delay(20);
+  }
+  tpose();
 }
 
 void setup() {
@@ -258,14 +372,26 @@ void setup() {
   });
   Serial.println("Server web avviato.");
 
-  // == SETUP ==
+  // == ENDPOINTS ==
+  /*server.on("/greet", [](){
+    server.send(200, "text/html", getHTML());
+  });
+  server.on("/sit", [](){
+      status = status ? false : true;
+      status ? raise() : down();
+  });*/
 
-  
-  
+
+  // == SETUP ==
+  downSmooth();
+  riseSmooth();
+  delay(2000);
+  resetFromRise();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   server.handleClient();
-
 }
+
